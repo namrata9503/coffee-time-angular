49781558c20fe112f4dfad3a6289714e6451efe7
namrata <namrata@pxlwidgets-heroes.com>
2021-06-10 12:27:17 +0200

    tested navbar button

--- src/app/components/header/navbar/navbar.component.spec.ts
+++ src/app/components/header/navbar/navbar.component.spec.ts
@@ -1,25 +1,32 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { NavbarComponent } from './navbar.component';
 
 describe('NavbarComponent', () => {
-  let component: NavbarComponent;
-  let fixture: ComponentFixture<NavbarComponent>;
+    let component: NavbarComponent;
+    let fixture: ComponentFixture<NavbarComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ NavbarComponent ]
-    })
-    .compileComponents();
-  });
+    beforeEach(async () => {
+        await TestBed.configureTestingModule({
+            declarations: [NavbarComponent],
+        })
+            .compileComponents();
+    });
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(NavbarComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    beforeEach(() => {
+        fixture = TestBed.createComponent(NavbarComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
+    it('should check menu button clicked', async(() => {
+        fixture.detectChanges();
+        spyOn(component, 'toggleNavbar');
+        const menuButton = fixture.debugElement.nativeElement.querySelector('button');
+        menuButton.click();
+        expect(menuButton).toBeTruthy();
+        expect(component.toggleNavbar).toHaveBeenCalled();
+    }));
 });
--- src/app/pages/users/users.component.spec.ts
+++ src/app/pages/users/users.component.spec.ts
@@ -1,23 +1,119 @@
 import { HttpClientTestingModule } from '@angular/common/http/testing';
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { async, ComponentFixture, fakeAsync, TestBed, tick } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
+import { UserDetailsService } from '@services/users-details.service';
+import { of } from 'rxjs';
 import { UsersComponent } from './users.component';
 
+const mockUserDetails = {
+    user: 'ellis',
+    totalPayedAmount: 24,
+    costOfAllOrders: 65.75,
+    owed: 41.75,
+    creditSafeScore: 2,
+    orders: [
+        {
+            user: 'ellis',
+            drink: 'long black',
+            size: 'small',
+        },
+        {
+            user: 'ellis',
+            drink: 'mocha',
+            size: 'small',
+        },
+        {
+            user: 'ellis',
+            drink: 'mocha',
+            size: 'small',
+        },
+        {
+            user: 'ellis',
+            drink: 'latte',
+            size: 'small',
+        },
+        {
+            user: 'ellis',
+            drink: 'flat white',
+            size: 'small',
+        },
+        {
+            user: 'ellis',
+            drink: 'supermochacrapucaramelcream',
+            size: 'huge',
+        },
+        {
+            user: 'ellis',
+            drink: 'short espresso',
+            size: 'small',
+        },
+        {
+            user: 'ellis',
+            drink: 'short espresso',
+            size: 'small',
+        },
+        {
+            user: 'ellis',
+            drink: 'flat white',
+            size: 'large',
+        },
+        {
+            user: 'ellis',
+            drink: 'supermochacrapucaramelcream',
+            size: 'ultra',
+        },
+        {
+            user: 'ellis',
+            drink: 'short espresso',
+            size: 'small',
+        },
+        {
+            user: 'ellis',
+            drink: 'flat white',
+            size: 'small',
+        },
+        {
+            user: 'ellis',
+            drink: 'short espresso',
+            size: 'small',
+        },
+        {
+            user: 'ellis',
+            drink: 'supermochacrapucaramelcream',
+            size: 'large',
+        },
+        {
+            user: 'ellis',
+            drink: 'short espresso',
+            size: 'small',
+        },
+        {
+            user: 'ellis',
+            drink: 'supermochacrapucaramelcream',
+            size: 'ultra',
+        },
+    ],
+};
 describe('UsersComponent', () => {
     let component: UsersComponent;
     let fixture: ComponentFixture<UsersComponent>;
+    let detailsService: UserDetailsService;
+    const mockUsers = mockUserDetails;
 
     beforeEach(async () => {
         await TestBed.configureTestingModule({
             declarations: [UsersComponent],
+            providers: [
+                { provide: UserDetailsService },
+            ],
             imports: [RouterTestingModule, HttpClientTestingModule],
 
-        })
-            .compileComponents();
+        }).compileComponents();
     });
 
     beforeEach(() => {
         fixture = TestBed.createComponent(UsersComponent);
+        detailsService = TestBed.get(UserDetailsService);
         component = fixture.componentInstance;
         fixture.detectChanges();
     });
@@ -32,5 +128,15 @@ describe('UsersComponent', () => {
         expect(component.getUsers).toBeTruthy();
         expect(component.getUsers).toHaveBeenCalled();
         expect(component.users$).not.toBeNull();
+    }));
+
+    it('testing subscribe method', fakeAsync(() => {
+        // eslint-disable-next-line max-len
+        const detialSpy = spyOn(detailsService, 'getUserDetails').withArgs(mockUsers.user).and.returnValue(of(mockUsers));
+        const subSpy = spyOn(detailsService.getUserDetails(mockUsers.user), 'subscribe');
+        component.ngOnInit();
+        tick();
+        expect(detialSpy).toBeTruthy();
+        expect(subSpy).toBeTruthy();
     }));
 });
